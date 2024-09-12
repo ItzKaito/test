3c31b3fe92cb296db56808f7a00e09d9c03d1045
ItzKaito <frenzdominiccartajena@gmail.com>
2024-09-12 19:53:33 +0800

    Add files via upload

---
+++ codingwithfrenz.java
@@ -0,0 +1,13 @@
+package com;
+
+import java.util.Date;
+
+public class codingwithfrenz {
+    
+    public static void main(String[] args) {
+        byte age = 19;
+        Date now = new Date();
+        System.out.println(now);
+    }
+    
+}
